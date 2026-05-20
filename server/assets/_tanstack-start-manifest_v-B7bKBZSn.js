b0eea9dfa49a7ddbb79abe914b43e27c80604873
navneetsn18 <navneetsn18@gmail.com>
2026-05-20 13:46:08 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-B7bKBZSn.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/navneetsn18/Downloads/bio/bio-data-builder/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-CWieIksn.js"] }, "/": { filePath: "/Users/navneetsn18/Downloads/bio/bio-data-builder/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BE5T43xY.js"] } }, clientEntry: "/assets/index-CWieIksn.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-CM0TSfQt.js";
+import "node:events";
+export {
+  w as default
+};
